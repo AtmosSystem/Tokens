d2ecb7def5655464d523fa3b23f0752833e59133
Santiago de Pedro <santiago.pedro23@gmail.com>
2020-01-22 11:25:08 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [atmos-kernel "1.3"]]
+                 [atmos-kernel "1.5"]]
   :repositories [["releases" {:url           "https://clojars.org/repo"
                               :username      :env/CLOJAR_USERNAME
                               :password      :env/CLOJAR_PASSWORD
