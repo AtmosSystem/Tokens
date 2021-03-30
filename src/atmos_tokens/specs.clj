717452639360ed8395492e9b6df57ddf0728c2a6
Santiago de Pedro <santiago.pedro23@gmail.com>
2021-03-30 11:10:42 -0400

    feat: Refactoring service.

--- src/atmos_tokens/specs.clj
+++ src/atmos_tokens/specs.clj
@@ -1,28 +1,39 @@
 (ns atmos-tokens.specs
   (:require [clojure.spec.alpha :as s]
-            [atmos-kernel.serializer.core :refer [is-valid-serializer-map? is-valid-de-serializer-map?]]))
+            [atmos-kernel.serializer.core :refer [is-valid-serializer-map? is-valid-de-serializer-map?]]
+            [clojure.string :as string]))
 
-(s/def ::type string?)
+(def token-types ["access_token" "refresh_token"])
+(s/def ::token-type (s/and string? (fn [token-type] (some #(= token-type %) token-types))))
+
+(s/def ::type ::token-type)
+(s/def ::validation_type ::token-type)
 (s/def ::entity string?)
 (s/def ::token string?)
-(s/def ::validation-type string?)
-(s/def ::extra-data (s/map-of keyword? any?))
+(s/def ::extra_data (s/map-of keyword? any?))
 
-(s/def ::token-spec (s/keys :req-un [::type ::entity ::extra-data]))
-(s/def ::token-validation-spec (s/keys :req-un [::token ::validation-type ::extra-data]))
+(s/def ::token-spec (s/keys :req-un [::type ::entity]
+                            :opt-un [::extra_data]))
+
+(s/def ::token-validation-spec (s/keys :req-un [::token ::validation_type]
+                                       :opt-un [::extra_data]))
 
 (def serialize-token-map {:type        :type
                           :access_type :access-type
                           :token       :token})
 
+(defn de-serialize-type
+  [token-type]
+  (-> token-type (string/replace #"_" "-") keyword))
+
 ; Map to de serialize token data.
 (def de-serialize-token-map {:data-spec ::token-spec
                              :fields    {:entity     :entity
-                                         :type       :type
+                                         :type       de-serialize-type
                                          :extra-data :extra_data}})
 
 ; Map to de serialize token validation data.
 (def de-serialize-token-validation-map {:data-spec ::token-validation-spec
                                         :fields    {:token           :token
-                                                    :validation-type :validation_type
+                                                    :validation-type de-serialize-type
                                                     :extra-data      :extra_data}})
